83db2ca6fc92cc1dd2e568589dc74b724d6d33b2
webtopc2021 <webtopc2021@gmail.com>
2024-02-18 07:13:31 +0900

    email

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class TestMail extends Mailable
 {
     use Queueable, SerializesModels;
-
     public $link;
     public $subject;
-
     /**
      * Create a new message instance.
      *
@@ -24,14 +25,42 @@ class TestMail extends Mailable
         $this->subject = $subject ? $subject : 'Pdfglue Document';
     }
 
+
     /**
-     * Build the message.
+     * Get the message envelope.
      *
-     * @return $this
+     * @return \Illuminate\Mail\Mailables\Envelope
      */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Test Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
     public function build()
     {
-        return $this->subject($this->subject)
-                    ->view('mail.test');
+        return $this->subject($this->subject)->view('mail.test');
     }
 }
