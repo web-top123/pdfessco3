543097331d13f928be7926a5ac6df8cae1ba54d6
webtopc2021 <webtopc2021@gmail.com>
2024-02-22 11:04:32 +0900

    upload file test

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -74,7 +74,7 @@ class FilesController extends Controller
        try{
         if(\Auth::check())
         {
-            $file_name = str_replace(' ', '_', preg_replace('/[^A-Za-z0-9_\-]/', '', $request->title)).'pdf';
+            $file_name = str_replace(' ', '_', preg_replace('/[^A-Za-z0-9_\-\s]/', '', $request->title)).'pdf';
             $path = $path = $request->file->storeAs('public/uploaded', $file_name);
             $file = \Auth::user()->files()->create([
                 'name' => $request->title,
